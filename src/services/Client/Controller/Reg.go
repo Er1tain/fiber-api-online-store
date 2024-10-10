ced0583084abcf210d9d66286ece9cc8c57a6cab
unknown <sanyapridava@mail.ru>
2024-10-10 22:54:08 +0300

    fix: now /reg handler return pair <Tokem: jwt>

--- src/services/Client/Controller/Reg.go
+++ src/services/Client/Controller/Reg.go
@@ -2,9 +2,12 @@ package client
 
 import (
 	client_models "api/src/services/Client/Models"
+	"api/src/shared"
+	"api/src/shared/responses"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 // Эндпоинт регистрации
@@ -12,15 +15,26 @@ func (controller ClientController) reg(service fiber.Router) {
 	service.Post("/reg", func(c *fiber.Ctx) error {
 		log.Println("Выполнен запрос на регистрацию!")
 
-		response := new(client_models.Client)
-		err := c.BodyParser(&response)
+		request := new(client_models.Client)
+		err := c.BodyParser(&request)
 		if err != nil {
 			return c.SendStatus(fiber.StatusBadRequest)
 		}
 
 		//Создание учётной записи в БД
-		if client_models.CreateClient(*response) {
-			return c.Status(fiber.StatusOK).JSON(response)
+		if client_models.CreateClient(*request) {
+			//Создание jwt-токена
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+				"email": request.Email, "surname": request.Surname, "name": request.Name,
+			})
+
+			tokenString, err := token.SignedString(shared.HmacSampleSecret)
+
+			if err == nil {
+				return c.Status(fiber.StatusOK).JSON(responses.SendJWT{
+					Token: tokenString,
+				})
+			}
 		}
 
 		return c.Status(fiber.ErrBadRequest.Code).SendString("Данный email уже используется")
