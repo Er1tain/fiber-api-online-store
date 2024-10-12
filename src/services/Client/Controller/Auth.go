752ec107ab4fab0334c3195c46d54b68c64d844b
unknown <sanyapridava@mail.ru>
2024-10-12 22:20:39 +0300

    feat: Created handler what giving jwt

--- src/services/Client/Controller/Auth.go
+++ src/services/Client/Controller/Auth.go
@@ -1,16 +1,44 @@
 package client
 
-import "github.com/gofiber/fiber/v2"
+import (
+	client_models "api/src/services/Client/Models"
+	"api/src/shared"
+	"api/src/shared/responses"
+	"log"
+	"time"
 
-type AuthData struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
 
 // Эндпоинт аутентификации
 func (controller ClientController) auth(service fiber.Router) {
 	service.Post("/auth", func(c *fiber.Ctx) error {
+		log.Println("Выполнен запрос на аутентификацию!")
 
-		return c.SendString("Вы успешно вошли!")
+		request := new(client_models.AuthData)
+		err := c.BodyParser(&request)
+		if err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+
+		//Поиск в БД записи с соответствующими почтой и паролем
+		surname, name, res := client_models.FindClient(request.Email, request.Password)
+		if res {
+			//Создание jwt-токена
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+				"email": request.Email, "surname": surname, "name": name, "createdAt": time.Now(),
+			})
+
+			tokenString, err := token.SignedString(shared.HmacSampleSecret)
+
+			if err == nil {
+				return c.Status(fiber.StatusOK).JSON(responses.SendJWT{
+					Token: tokenString,
+				})
+			}
+		}
+
+		return c.Status(fiber.ErrBadRequest.Code).SendString("Неверный логин и/или пароль!")
 	})
 }
