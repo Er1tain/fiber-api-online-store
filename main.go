22cf83218a38a79936c9e9b36a72976ad58badc7
unknown <sanyapridava@mail.ru>
2024-10-12 20:31:13 +0300

    feat: begin work with swagger

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 )
 
+// @title Fiber-API-Online-Store
+// @version 1.0
+// @description Golang GoFiber swagger auto generate step by step by swaggo
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8000
+// @BasePath /
 func main() {
 	api := fiber.New()
-
+	api.Get("/docs/*", swagger.HandlerDefault)
 	client.Start(api)
 
 	log.Fatal(api.Listen(":8000"))
